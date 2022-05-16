9460e071dabb035925002c93670e38f61e5916e0
Nick <Nick_II2000@mail.ru>
2022-05-16 20:40:13 +0600

    4-th commit

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 
 const box = document.querySelector('.box');
 
-const newHeight = 0;
+const newHeight = 300;
 const newWidth = 400;
 
 function changeParams(elem, h, w) {
     elem.style.height = `${h ?? 200}px`;
     elem.style.width = `${w ?? 200}px`;
+    elem.innerHTML = (h ?? 200) * (w ?? 200);
 }
 
 changeParams(box, newHeight, newWidth);
