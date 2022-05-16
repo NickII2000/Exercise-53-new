5a81c203a09b22e105916f0b90385f72645a6e1d
Nick <Nick_II2000@mail.ru>
2022-05-16 20:34:38 +0600

    2-th commit

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ const newHeight = 100;
 const newWidth = 400;
 
 function changeParams(elem, h, w) {
-    elem.style.height = `${h}px`;
-    elem.style.width = `${w}px`;
+    elem.style.height = `${h ?? 200}px`;
+    elem.style.width = `${w ?? 200}px`;
 }
 
 changeParams(box, newHeight, newWidth);
+
+let userName;
+console.log(userName ?? 'User');
